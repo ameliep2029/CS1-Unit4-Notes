7c8ecd58a1c163975e149b3f1098d3ca21a86dc2
ameliep2029 <amelie.proietti2029@gbwl.org>
2026-04-15 14:25:00 +0000

    Opperators

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("hello world");
+
+
+//  MATH OPERATIONS (with numbers) 
+
+let age = 18;
+let ageInTen = age + 10 ;
+let ageDogYears = age / 8;
+console.log(age); 
+console.log(ageInTen);
+console.log(ageDogYears);
+// You can do math with +, -, *, / 
+
+
+// Combine Strings with CONCATENATION 
+console.log("My Age is " + "30");
+//Combine strings with variables 
+console.log("My Age is" + age); 
+console.log("In ten years I will be " + ageInTen);
+console.log("My age in dog years is " + ageDogYears);
+// using the plus sign with at least one "String"
+// means you're combining (concatenating) not doing addition
